b384771a96c406ca64531d3e4e7268c969038ff3
Leo Kamhoot <leo@innovit.co.za>
2024-09-29 09:18:05 +0200

    added fft rerefernces

---
+++ FFT-Ocean 2.0/src/shaders/utils/fft.glsl
@@ -0,0 +1,4 @@
+// https://github.com/rreusser/glsl-fft?tab=readme-ov-file
+// https://github.com/Themaister/GLFFT
+// https://www.bealto.com/gpu-fft_opencl-1.html
+// https://github.com/bane9/OpenGLFFT //!!!!
